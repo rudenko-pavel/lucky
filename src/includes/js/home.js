a773891bcc5a053e6b352ccfe76f4b767e0610c4
FASTANDSMART\admin <pavel.v.rudenko@gmail.com>
2019-08-12 22:50:38 +0300

    apply MDB-styles
    DONE:
    - add touch to galery on `HOME`-page
    - apply MDB-styles for carousel on `HOME`-page
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    NEXT GOALS:
    - tooltip doesn't work in mobile
    - add page `ATHLETES:id`
    - add page `EVENTS:id`
    - add CSS for img:hover (.one-event)
    -`HOME` - add running blocks with donuts and data
    
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    in FUTURE:
    - add page `AWARDS`
    - add page `GALLERY` with all photos
    - `EVENTS`, `ATHLETES` - download not all images. Some images download after click

--- src/includes/js/home.js
+++ src/includes/js/home.js
@@ -1,86 +1,39 @@
 $.runItems="";
 $.runnings="";
-$.storageImg = "dist/img/timeline/";
+$.storageImg = "dist/img/events/";
 $.modalImg = "";
+$.newIndicator = "";
 
 $.getJSON( "dist/includes/json/events.json", function( data ) {
     $.runnings = data.data;
 })
 .done(function(){
     if ($.runItems.length==0){
+        var newItem = "";
+        var newIndicator ="";
         $.each( $.runnings, function(key) {
-
-        var newItem ='<div data-time="'+$.runnings[key]["runDate"]+'" class="timeline-item">'+
-            '<div class="timeline-visual col-12 col-sm-12 col-md-6" data-toggle="modal" data-target="#carousel'+$.runnings[key]["runId"]+'">'+
-                '<img src="'+$.storageImg+$.runnings[key]["runId"]+'.jpg" alt="" data-target="#carousel'+$.runnings[key]["runId"]+'" data-slide-to="0">'+
-            '</div>'+
-            '<div class="timeline-detail col-12 col-sm-12 col-md-6">'+
-                '<h5>'+$.runnings[key]["name"]+'</h5>'+
-                '<div class="niceDate">'+$.runnings[key]["runDate"]+'</div>'+
-                '<div>'+$.runnings[key]["descriptions"]+'</div>'+
-            '</div>'+
-            '</div>';
-            $.runItems = $.runItems + newItem;
-
-        var newModalCollection = "";
-        var newModalCollectionStart ='<div class="modal fade" id="carousel'+$.runnings[key]["runId"]+'" tabindex="-1" role="dialog" aria-hidden="true">'+
-            '<div class="modal-dialog" role="document">'+
-                '<div class="modal-content">'+
-                    '<div class="modal-header">'+
-                        '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
-                            '<span aria-hidden="true">&times;</span>'+
-                        '</button>'+
-                    '</div>'+
-                    '<div class="modal-body">'+        
-                        '<div id="images_'+$.runnings[key]["runId"]+'" class="carousel slide" data-ride="carousel">'+
-                            '<div class="carousel-inner">';
-
-        var newModalCollectionMiddle = "";
-        for (i=1;i<=$.runnings[key]["countImages"]; i++){
-            var addClass = "";
-            if (i==1) addClass=" active"; else addClass = ""
-            newModalCollectionMiddle += '<div class="carousel-item'+addClass+'">'+
-                                    '<img class="d-block w-100" src="'+$.storageImg+$.runnings[key]["runId"]+'/'+i+'.jpg">'+
-                                '</div>';
-        }
-                                
-        
-        var newModalCollectionEnd ='</div>'+ 
-                            '<a class="carousel-control-prev" href="#images_'+$.runnings[key]["runId"]+'" role="button" data-slide="prev">'+
-                                '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'+
-                                '<span class="sr-only">Previous</span>'+
-                            '</a>'+
-                            '<a class="carousel-control-next" href="#images_'+$.runnings[key]["runId"]+'" role="button" data-slide="next">'+
-                                '<span class="carousel-control-next-icon" aria-hidden="true"></span>'+
-                                '<span class="sr-only">Next</span>'+
-                            '</a>'+       
-                        '</div>'+
-                    '</div>'+
-                '</div>'+
-            '</div>'+
-            '</div>';
-            
-        newModalCollection = newModalCollectionStart + newModalCollectionMiddle + newModalCollectionEnd;
-            
-        $.modalImg = $.modalImg +newModalCollection;
-
-
+            var addClass="";
+            if (key==0) addClass=" active";
+            newItem=
+                "<div data-currid='"+key+"' class='timeline-item carousel-item"+addClass+"'>"+
+                    "<div class='timeline-visual col-12 col-sm-12 col-md-6'>"+
+                        "<img class='card-img-top' src='"+$.storageImg+$.runnings[key]["runId"]+".jpg' alt='"+$.runnings[key]["name"]+"'>"+
+                    "</div>"+
+                    "<div class='card-body timeline-detail col-12 col-sm-12 col-md-6'>"+
+                        "<h5>"+$.runnings[key]["name"]+"</h5>"+
+                        "<div class='niceDate'>"+$.runnings[key]["runDate"]+"</div>"+
+                        "<div>"+$.runnings[key]["descriptions"]+"</div>"+
+                        "<a class='btn btn-info btn-block my-4'>Подробнее</a>"+
+                    "</div>"+
+                "</div>";
+            newIndicator ="<li data-target='#carouselEvents' data-slide-to='"+key+"' class='btn "+addClass+"'><div class='inTimeline'>"+$.runnings[key]["runDate"]+"</div></li>";
+                $.runItems = $.runItems + newItem;
+                $.newIndicator = $.newIndicator + newIndicator;
         });
-        $("#actionsContent").prepend($.runItems);
-        $('#modalSegment').append($.modalImg);
+        $("#listbox-events").prepend($.runItems);
+        $("#carousel-indicators").prepend($.newIndicator);
 
-        $.getScript("./dist/includes/js/vendor/timeline.min.js",function(){
-            
-            $('.js-timeline').Timeline(
-                {
-                    itemClass: 'box-item',
-                    dotsPosition: 'bottom',
-                    startItem: 'first',
-                    autoplay: true,
-                    autoplaySpeed: 10000,
-                }
-            );
-        });
+
     }
 })
 .done(function(){
@@ -128,6 +81,31 @@ $.repeatCount = 0;
     $.repeatCount++;
  };
  $(document).ready(function(){
-     setInterval(runEffect,10000);
- });
+    setInterval(runEffect,10000);
+
+    $('.carousel').carousel({
+        interval: 0,
+        touch: true,
+        ride: false
+    })
+    
+    $.maxWidth = $( "#carouselEvents" ).width();
+    $.positionLeft = $.maxWidth/2;   
+    console.log("0 maxWidth",$.maxWidth);     
+    console.log("$.positionLeft",$.positionLeft+"px");
+    $( "#carousel-indicators" ).css("left",$.positionLeft+"px");
+
+    $("#carouselEvents").bind("slid.bs.carousel",function(){
+    
+        console.log("maxWidth",$.maxWidth);
+        $.positionLeft = $.maxWidth/2 + 50 - ($( ".carousel-item.active" ).data("currid")+1)*106;   
+        var newValue = $.positionLeft+"px";
+        
+        var left = $('#carousel-indicators').left;
+        $("#carousel-indicators").css({left:left}).animate({"left":newValue}, "slow");
+        $( "#carousel-indicators" ).css("left",$.positionLeft);
+     });
+
+
+});
 
